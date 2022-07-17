49c2d68fa5b15c050aed615c8293b98904f8f21b
ddjaell <ljy2905.92@gmail.com>
2022-07-18 02:34:48 +0900

    개인정보입력

--- src/components/member/register/PersonalInfo.vue
+++ src/components/member/register/PersonalInfo.vue
@@ -6,7 +6,9 @@
                 <v-chip>이메일</v-chip>            
             </v-col>
             <v-col>
-                <v-text-field label="이메일"></v-text-field>
+                <v-text-field label="이메일"
+                :rules="emailRule"
+                v-model="memberEmail"></v-text-field>
             </v-col>
         </v-row>
         <v-row align="center"
@@ -15,7 +17,10 @@
                 <v-chip>비밀번호</v-chip>            
             </v-col>
             <v-col>
-                <v-text-field label="비밀번호"></v-text-field>
+                <v-text-field label="비밀번호"
+                :rules="passwordRule"
+                v-model="memberPassword"
+                type="password"></v-text-field>
             </v-col>
         </v-row>
         <v-row align="center"
@@ -24,7 +29,10 @@
                 <v-chip>비밀번호 확인</v-chip>            
             </v-col>
             <v-col>
-                <v-text-field label="비밀번호 확인"></v-text-field>
+                <v-text-field label="비밀번호 확인"
+                :rules="passwordCheckRule"
+                v-model="memberPasswordCheck"
+                type="password"></v-text-field>
             </v-col>
         </v-row>
         <v-row align="center"
@@ -35,14 +43,39 @@
 </template>
 <script>
     export default{
+        data () {
+            return{
+                memberEmail : null,
+                memberPassword : null,
+                memberPasswordCheck : null,
+                emailRegEx : /^[a-zA-Z0-9+-\\_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+                passwordRegEx : /^(?=.*[a-z])(?=.*[A-Z])(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&]{8,}/,
+                emailRule : [v => this.emailRegEx.test(v) || '이메일 형식을 확인해주세요.'],
+                passwordRule : [v => this.passwordRegEx.test(v) || '비밀번호는 대문자, 소문자, 특수문자 포함 최소 8자입니다.'],
+                passwordCheckRule : [v => v === this.memberPassword || '비밀번호가 다릅니다.'],
+            }
+        },
         methods : {
             goToDelivery() {
                 if(this.validateForm()){
-                    this.$router.push("/delivery");
+                    this.setPersonalInfo()
+                    this.$router.push("/delivery")
                 }
             },
             validateForm() {
-                return true;
+                if(!this.emailRegEx.test(this.memberEmail)){
+                    alert("메일 확인 부탁드립니다.")
+                    return false
+                }
+                if(!this.passwordRegEx.test(this.memberPassword) || this.memberPasswordCheck !== this.memberPassword) {
+                    alert("비밀번호 확인 부탁드립니다.")
+                    return false
+                }
+                return true
+            },
+            setPersonalInfo() {
+                this.$store.commit('setEmail', this.memberEmail)
+                this.$store.commit('setPassword', this.memberPassword)
             }
         }
     }
